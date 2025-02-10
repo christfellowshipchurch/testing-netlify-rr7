e1d45581d42f9690d423c0a4cd24faeed2af9e02
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2025-02-10 15:03:46 -0500

    Test Commit

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -23,6 +23,8 @@ export function Welcome() {
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
               What&apos;s next?
+              <br />
+              Testing Netlify deployments
             </p>
             <ul>
               {resources.map(({ href, text, icon }) => (
